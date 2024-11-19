48e1abc1cda9da8a7bcdd7f0cf623dd698ad3e03
Shubhajit <shubjitgithub@gmail.com>
2024-11-19 19:05:48 +0530

    Di Using Setter & Getter

---
+++ spring_03/src/main/java/com/example/Bus.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class Bus implements Vehicle {
+    private Fare fare;
+
+    public Bus() {
+        
+    }
+
+    // setter di
+    public void setFareObject(Fare fare) {
+        this.fare = fare;
+}
+
+    public void getMileage() {
+        System.out.println("Bus mileage: 8 km/l");
+    }
+
+    public void getFare() {
+        System.out.println("Bus fare: "+fare.fareforBus());
+    }
+}
