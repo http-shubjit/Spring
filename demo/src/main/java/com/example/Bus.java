8019e3a6b067aaf880d6674c30317184cb39f108
Shubhajit <shubjitgithub@gmail.com>
2024-11-19 16:02:18 +0530

    After Spring Using Ioc and Di

--- demo/src/main/java/com/example/Bus.java
+++ demo/src/main/java/com/example/Bus.java
@@ -1,7 +1,6 @@
 package com.example;
 
 public class Bus implements Vehicle {
-    @Override
     public void getMileage() {
         System.out.println("Bus mileage: 8 km/l");
     }
--- demo/src/main/java/com/example/Car.java
+++ demo/src/main/java/com/example/Car.java
@@ -1,7 +1,7 @@
 package com.example;
 
 public class Car implements Vehicle {
-    @Override
+    
     public void getMileage() {
         System.out.println("Car mileage: 15 km/l");
     }
